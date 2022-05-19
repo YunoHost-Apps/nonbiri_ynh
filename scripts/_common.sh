560edf8b69c058b7c156e50cc2eea3b4ce777a01
ericgaspar <junk.eg@free.fr>
2022-05-19 10:22:18 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="gcc nodejs yarn golang"
+pkg_dependencies=""
 
 #=================================================
 # PERSONAL HELPERS
